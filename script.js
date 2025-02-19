ca4fdd492b7a97f5873bcd83c59aa88b339e78d1
hhli03 <lik954@newschool.edu>
2025-02-18 22:28:52 -0500

    draft2

--- script.js
+++ script.js
@@ -8,20 +8,20 @@ document.addEventListener("DOMContentLoaded", function () {
         const images = track.querySelectorAll("img");
 
         let index = 0; // Start at the first image
-        const numVisible = parseInt(carousel.dataset.images, 10); // Get how many images should be visible
-        const imgWidth = carousel.clientWidth / numVisible; // Get width for each image
+        const numVisible = parseInt(carousel.dataset.images, 10); // Number of visible images
+        const imgWidth = carousel.clientWidth / numVisible; // Width for each image
 
         function updateCarousel() {
             track.style.transform = `translateX(-${index * imgWidth}px)`;
         }
 
         prevButton.addEventListener("click", () => {
-            index = (index > 0) ? index - numVisible : images.length - numVisible;
+            index = (index > 0) ? index - 1 : images.length - numVisible; // Move only 1 image
             updateCarousel();
         });
 
         nextButton.addEventListener("click", () => {
-            index = (index < images.length - numVisible) ? index + numVisible : 0;
+            index = (index < images.length - numVisible) ? index + 1 : 0; // Move only 1 image
             updateCarousel();
         });
 
